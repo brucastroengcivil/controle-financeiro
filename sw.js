413916c3b68e43ee780200e878da3d6322ede775
brucastroengcivil <57329098+brucastroengcivil@users.noreply.github.com>
2026-09-19 11:27:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * o service worker mudou e a limpar o cache antigo automaticamente.
  */
 
-const CACHE_NAME = "controle-financeiro-shell-v4";
+const CACHE_NAME = "controle-financeiro-shell-v5";
 const APP_SHELL = ["./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
